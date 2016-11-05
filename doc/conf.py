ab84f7d2184f482930203b34a6576d2b4918864d
Matthias Geier <Matthias.Geier@gmail.com>
2016-11-05 16:00:52 +0100

    DOC: Switch to julia theme

--- doc/conf.py
+++ doc/conf.py
@@ -5,6 +5,8 @@ import os
 # Use sphinx-quickstart to create your own conf.py file!
 # After that, you have to edit a few things.  See below.
 
+import juliadoc
+
 # Select nbsphinx and, if needed, other Sphinx extensions:
 extensions = [
     'nbsphinx',
@@ -124,6 +126,9 @@ except Exception:
 # -- Options for HTML output ----------------------------------------------
 
 html_title = project + ' version ' + release
+html_theme = 'julia'
+html_theme_path = [juliadoc.get_theme_dir()]
+html_sidebars = juliadoc.default_sidebars()
 
 # -- Options for LaTeX output ---------------------------------------------
 
