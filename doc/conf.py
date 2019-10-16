23095f6df9ba8a7b8c3ef2e12ef91341578317e0
Matthias Geier <Matthias.Geier@gmail.com>
2019-10-16 22:00:11 +0200

    DOC: Switch to pyviz theme

--- doc/conf.py
+++ doc/conf.py
@@ -124,6 +124,7 @@ except Exception:
 # -- Options for HTML output ----------------------------------------------
 
 html_title = project + ' version ' + release
+html_theme = 'sphinx_pyviz_theme'
 
 # -- Options for LaTeX output ---------------------------------------------
 
